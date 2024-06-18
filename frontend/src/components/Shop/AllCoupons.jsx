f029ca196265f59d5592cc33799dfa4e9b22c17f
nayakchaithra5@gmail.com <nayakchaithra5@gmail.com>
2024-06-19 00:29:29 +0530

    refactor: Remove coupon code restriction for selected products

--- frontend/src/components/Shop/AllCoupons.jsx
+++ frontend/src/components/Shop/AllCoupons.jsx
@@ -17,10 +17,8 @@ const AllCoupons = () => {
   const [Coupons, setCoupons] = useState([]);
   const [minAmount, setMinAmout] = useState(null);
   const [maxAmount, setMaxAmount] = useState(null);
-  const [selectedProducts, setSelectedProducts] = useState(null);
   const [value, setValue] = useState(null);
   const { seller } = useSelector((state) => state.seller);
-  const { products } = useSelector((state) => state.products);
 
   const dispatch = useDispatch();
 
@@ -58,7 +56,6 @@ const AllCoupons = () => {
           name,
           minAmount,
           maxAmount,
-          selectedProducts,
           value,
           shopId: seller._id,
         },
@@ -75,18 +72,29 @@ const AllCoupons = () => {
   };
 
   const columns = [
-    { field: "id", headerName: "Id", minWidth: 150, flex: 0.7 },
+    {
+      field: "id",
+      headerName: "Id",
+      minWidth: 150,
+      flex: 0.7,
+      align: "center",
+      headerAlign: "center",
+    },
     {
       field: "name",
       headerName: "Coupon Code",
       minWidth: 180,
       flex: 1.4,
+      align: "center",
+      headerAlign: "center",
     },
     {
       field: "price",
       headerName: "Value",
       minWidth: 100,
       flex: 0.6,
+      align: "center",
+      headerAlign: "center",
     },
     {
       field: "Delete",
@@ -104,6 +112,8 @@ const AllCoupons = () => {
           </>
         );
       },
+      align: "center",
+      headerAlign: "center",
     },
   ];
 
@@ -124,7 +134,7 @@ const AllCoupons = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="w-full mx-8 pt-1 mt-10 bg-white">
+        <div className="w-full mx-8 pt-1 mt-10">
           <div className="w-full flex justify-end">
             <div
               className={`${styles.button} !w-max !h-[45px] px-3 !rounded-[5px] mr-3 mb-3`}
@@ -142,7 +152,7 @@ const AllCoupons = () => {
           />
           {open && (
             <div className="fixed top-0 left-0 w-full h-screen bg-[#00000062] z-[20000] flex items-center justify-center">
-              <div className="w-[90%] 800px:w-[40%] h-[80vh] bg-white rounded-md shadow p-4">
+              <div className="w-[90%] 800px:w-[40%] h-[70vh] bg-white rounded-md shadow p-4">
                 <div className="w-full flex justify-end">
                   <RxCross1
                     size={30}
@@ -150,7 +160,7 @@ const AllCoupons = () => {
                     onClick={() => setOpen(false)}
                   />
                 </div>
-                <h5 className="text-[30px] font-Poppins text-center">
+                <h5 className="text-[18px] font-Poppins text-center">
                   Create Coupon code
                 </h5>
                 {/* create Coupon code */}
@@ -167,13 +177,13 @@ const AllCoupons = () => {
                       value={name}
                       className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                       onChange={(e) => setName(e.target.value)}
-                      placeholder="Enter your coupon code name..."
+                      placeholder="Enter your coupon code name"
                     />
                   </div>
                   <br />
                   <div>
                     <label className="pb-2">
-                      Discount Percentenge{" "}
+                      Discount Percentenge
                       <span className="text-red-500">*</span>
                     </label>
                     <input
@@ -183,7 +193,7 @@ const AllCoupons = () => {
                       required
                       className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                       onChange={(e) => setValue(e.target.value)}
-                      placeholder="Enter your coupon code value..."
+                      placeholder="Enter your coupon code value"
                     />
                   </div>
                   <br />
@@ -195,7 +205,7 @@ const AllCoupons = () => {
                       value={minAmount}
                       className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                       onChange={(e) => setMinAmout(e.target.value)}
-                      placeholder="Enter your coupon code min amount..."
+                      placeholder="Enter your coupon code min amount"
                     />
                   </div>
                   <br />
@@ -205,36 +215,18 @@ const AllCoupons = () => {
                       type="number"
                       name="value"
                       value={maxAmount}
-                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      className="mt-2 appearance-none block  w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                       onChange={(e) => setMaxAmount(e.target.value)}
-                      placeholder="Enter your coupon code max amount..."
+                      placeholder="Enter your coupon code max amount"
                     />
                   </div>
-                  <br />
-                  <div>
-                    <label className="pb-2">Selected Product</label>
-                    <select
-                      className="w-full mt-2 border h-[35px] rounded-[5px]"
-                      value={selectedProducts}
-                      onChange={(e) => setSelectedProducts(e.target.value)}
-                    >
-                      <option value="Choose your selected products">
-                        Choose a selected product
-                      </option>
-                      {products &&
-                        products.map((i) => (
-                          <option value={i.name} key={i.name}>
-                            {i.name}
-                          </option>
-                        ))}
-                    </select>
-                  </div>
+
                   <br />
                   <div>
                     <input
                       type="submit"
                       value="Create"
-                      className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      className="mt-2 appearance-none block w-full bg-[#243450] text-white px-3 h-[35px]  rounded-[3px] placeholder-gray-400  sm:text-sm"
                     />
                   </div>
                 </form>
