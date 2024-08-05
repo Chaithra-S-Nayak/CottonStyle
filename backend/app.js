a83f2d40a0de4fe80a8c29cda27ba739b3784d5b
Chaithra S Nayak <nayakchaithra5@gmail.com>
2024-08-05 22:17:17 +0530

    deploy: frontend deployment to Vercel

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://cotton-style-c.vercel.app",
     credentials: true,
   })
 );
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,1 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
+export const server = "https://cotton-style.vercel.app/";
